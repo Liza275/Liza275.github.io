d2b7e6be952147f3e02c3b5b9159463dee3bcc17
Liza_Voronkova <liza.voronkova.01@mail.ru>
2021-04-18 19:30:56 +0400

    is ready

---
+++ js/site.js
@@ -0,0 +1,25 @@
+let form_1 = document.getElementById('form');
+
+form_1.addEventListener("submit", function (event) {
+  event.preventDefault();
+new FormData(form_1);
+});
+
+form_1.addEventListener("formdata", event => {
+    const data = event.formData;
+    const values = [...data.values()];
+    console.log(values);
+    if(values[1].length==0){
+        Swal.fire('Введите номер телефона!','Проверьте введенные данные','error');
+        return;
+    }
+    if (values[0].length==0) {
+        Swal.fire('Введите имя и фамилию', 'Проверьте введенные данные', 'error');
+        return;
+    }
+    if (values[1].length < 6) {
+        Swal.fire('Номер слишком короткий!', '', 'warning');
+        return;
+    }
+    Swal.fire('Поздравляем!', 'Вы успешно прошли регистрацию!', 'success');
+});
